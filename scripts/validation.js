41658b7baa3a1e80e65b31b1e6bd137fbf666172
idrise4112 <166262404+idrise4112@users.noreply.github.com>
2024-10-03 14:08:20 -0600

    validation.js added

---
+++ scripts/validation.js
@@ -0,0 +1,33 @@
+// enabling validation by calling enableValidation()
+// pass all the settings on call
+
+function setEventListeners(formEl, options) {
+  const { inputSelector } = options;
+  const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  inputEls.forEach((inputEl) => {
+    inputEl.addEventListener("input", (e) => {
+      console.log(inputEl.validity);
+    });
+  });
+}
+function enableValidation(options) {
+  const formEls = [...document.querySelectorAll(options.formSelector)];
+  formEls.forEach((formEl) => {
+    formEl.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+
+    setEventListeners(formEl, options);
+  });
+}
+
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modaL__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+};
+
+enableValidation(config);
