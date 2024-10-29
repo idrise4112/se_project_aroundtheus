cb440933482fe62a9ecd33af05b779edaffb75df
idrise4112 <166262404+idrise4112@users.noreply.github.com>
2024-10-29 09:39:10 -0400

    new update

--- pages/FormVallidator.js
+++ pages/FormVallidator.js
@@ -1,14 +1,28 @@
-//class FormValidator {
-//constructor(settings, formElement) {
-//this._formElement = formElement;
-//console.log(this);
-//}
-//}
-//const settings = {
-//formSelector: ".modal__form",
-//inputSelector: ".modal__input",
-//submitButtonSelector: ".modal__button",
-//inactiveButtonClass: "modal__button_disabled",
-//inputErrorClass: "modal__input_type_error",
-//errorClass: "modal__error_visible",
-//  };//
+class FormValidator {
+  constructor(config, formElement) {
+    this._inputSelector = config.inputSelector;
+    this.SubmitButttonSelector = config.SubmitButttonSelector;
+    this._inactiveButtonClass = config.inactiveButtonClass;
+    this._inputErrorClass = config.inputErrorClass;
+    this._errorClass = config.errorClass;
+
+    this._element = formElement;
+  }
+
+  _showInputError(inputElement, errorMessage) {
+    const errorElement = this._element.querySelector(
+      `#${inputElement.id}-error`
+    );
+
+    inputElement.classList.add(this._inputErrorClass);
+    errorElement.textContent = "";
+    errorElement.classList.remove(this._errorClass);
+  }
+
+
+_hideInputError(inputElement) {
+  const errorMessage = this._element.querySelector(
+    `#${inputElement.id}-error`
+  );
+  inputElement.classList
+}
