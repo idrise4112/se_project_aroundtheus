f1982412d125663c30a4e15842fc2ce4e91ec904
idrise4112 <166262404+idrise4112@users.noreply.github.com>
2024-11-06 00:09:40 -0500

    new update

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,23 @@ const initialCards = [
 const cardData = {
   name: "Yosemite Valley",
   link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+
+  name: "Lake Louise",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg ",
+
+  name: "Bald Mountains",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+
+  name: "Latemar",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+
+  name: "Vanoise National Park",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+
+  name: "Lago di Braies",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
 };
+
 /*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 /*                                                                                                                                                                          */
 /*                                                                     Elements                                                                                                                                                 */
@@ -171,6 +187,11 @@ addCardFormElement.addEventListener("submit", handleAddCardSubmit);
 
 const handleImageClick = () => {
   const previewImage = document.querySelector(".card__image");
+  modalImage.src = cardData.link;
+  modalImage.alt = cardData.name;
+  previewText.textContent = cardData.name;
+  previewImageModal.classList.add("modal_opened");
+  openModal(previewImageModal);
 };
 
 initialCards.forEach((cardData) => {
